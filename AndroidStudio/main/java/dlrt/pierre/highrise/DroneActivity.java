e03692fc660263c12b94678a727a78a2e73913e8
PierreDlrt <pierre.dlrt@outlook.fr>
2020-01-26 15:22:43 +0100

    Add files via upload
    TCP reconnects automatically after connection lost

--- AndroidStudio/main/java/dlrt/pierre/highrise/DroneActivity.java
+++ AndroidStudio/main/java/dlrt/pierre/highrise/DroneActivity.java
@@ -1,13 +1,16 @@
 package dlrt.pierre.highrise;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.NetworkInfo;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.format.Formatter;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -20,7 +23,7 @@ public class DroneActivity extends AppCompatActivity {
     public static final String TAG = DroneActivity.class.getSimpleName();
 
     TcpClient mTcpClient;
-    TextView conStatus;
+    //TextView conStatus;
     EditText editText;
 
     int cpt=0;
@@ -30,7 +33,6 @@ public class DroneActivity extends AppCompatActivity {
     private WifiManager wifiManager;
     WifiReceiver receiverWifi;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,14 +41,15 @@ public class DroneActivity extends AppCompatActivity {
         final ConnectTask connectTask = new ConnectTask();
 
         Button btnConnect = findViewById(R.id.btnConnect);
+        Button btnDisconnect = findViewById(R.id.btnDisconnect);
         Button btnSend = findViewById(R.id.btnSend);
         editText = findViewById(R.id.editText);
-        conStatus = findViewById(R.id.conStatus);
+        //conStatus = findViewById(R.id.conStatus);
 
         wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
-        receiverWifi = new WifiReceiver(wifiManager);
+        receiverWifi = new WifiReceiver();
         registerReceiver(receiverWifi, intentFilter);
 
         if (!wifiManager.isWifiEnabled()) {
@@ -54,17 +57,31 @@ public class DroneActivity extends AppCompatActivity {
             wifiManager.setWifiEnabled(true);
         }
 
+        //while (wifiManager.getConnectionInfo()!=null)
+
         btnConnect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (wifiManager.getConnectionInfo()!=null) {
                     IP_addr = receiverWifi.IP_ADDR;
-                    if (connectTask.getStatus().equals(AsyncTask.Status.RUNNING)) {
+                    Log.d(TAG, "onClick: Button clicked");
+                    if (!connectTask.getStatus().equals(AsyncTask.Status.RUNNING)) {
+                        Log.d(TAG, "onClick: connectTask execution");
                         connectTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
                     }
-                }
-                else{
+                } else{
                     Toast.makeText(getApplicationContext(),"No connected device", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnDisconnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: disconnect clicked");
+                if(connectTask.getStatus().equals(AsyncTask.Status.RUNNING) && mTcpClient!=null){
+                    Log.d(TAG, "onClick: cancel task");
+                    mTcpClient.stopClient();
                 }
             }
         });
@@ -80,8 +97,7 @@ public class DroneActivity extends AppCompatActivity {
                         new SendMessageTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, message);
                     }
                     editText.setText("");
-                }
-                else {
+                } else {
                     Toast.makeText(getApplicationContext(),"No connected device", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -109,12 +125,14 @@ public class DroneActivity extends AppCompatActivity {
                 public void messageReceived(int message) {
                     //this method calls the onProgressUpdate
                     //publishProgress(message);
+                    Log.d(TAG, "messageReceived: AsyncTask launched");
                     cpt++;
                 }
             });
-            mTcpClient.run();
-
-            return null;
+            while(true){
+                while(wifiManager.getConnectionInfo()==null) {}
+                mTcpClient.run();
+            }
         }
 
         /*@Override
@@ -128,4 +146,31 @@ public class DroneActivity extends AppCompatActivity {
         }*/
     }
 
+    class WifiReceiver extends BroadcastReceiver {
+
+        private static final String TAG = "WifiReceiver";
+        public String IP_ADDR;
+
+        //WifiManager wifiManager;
+
+        public WifiReceiver(){//WifiManager wifiManager) {
+
+            //this.wifiManager = wifiManager;
+        }
+
+        public void onReceive(Context context, Intent intent) {
+
+            //String action = intent.getAction();
+
+            //if (WifiManager.NETWORK_STATE_CHANGED_ACTION.equals(action)) {
+            NetworkInfo networkInfo = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
+            if (networkInfo.isConnected()) {
+                IP_ADDR = Formatter.formatIpAddress(wifiManager.getDhcpInfo().serverAddress);
+            } else {
+                if (mTcpClient!=null){
+                    mTcpClient.stopClient();
+                }
+            }
+        }
+    }
 }
